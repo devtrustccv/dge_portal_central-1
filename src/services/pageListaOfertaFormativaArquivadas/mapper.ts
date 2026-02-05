04760708b124f1672b9e247fc415001d13b7713e
Kevin Sousa <kevinjunior.dev@outlook.com>
2026-02-05 21:10:17 -0100

    Update 5/Fev 21:10

--- src/services/pageListaOfertaFormativaArquivadas/mapper.ts
+++ src/services/pageListaOfertaFormativaArquivadas/mapper.ts
@@ -1,16 +1,17 @@
+
 import {IPageListaServicoData} from "@/services/page-list-oferta/type";
 export const mapper = (data: any): IPageListaServicoData | null => {
-    if (!data || !data.pageListaOfertaFormativasPrevista) return null;
+    if (!data || !data.pageListaOfertaFormativaArquivada) return null;
 
     return {
-        configs: data.pageListaOfertaFormativasPrevista.PageInfo.configs || {},
+        configs: data.pageListaOfertaFormativaArquivada.PageInfo.configs || {},
         headerImage: {
-            formats: data.pageListaOfertaFormativasPrevista.PageInfo.headerImage?.formats || {},
-            url: data.pageListaOfertaFormativasPrevista.PageInfo.headerImage?.url || "",
+            formats: data.pageListaOfertaFormativaArquivada.PageInfo.headerImage?.formats || {},
+            url: data.pageListaOfertaFormativaArquivada.PageInfo.headerImage?.url || "",
         },
-        title: data.pageListaOfertaFormativasPrevista.PageInfo.title || "",
-        subtitle: data.pageListaOfertaFormativasPrevista.PageInfo.subtitle || "",
-        description: data.pageListaOfertaFormativasPrevista.PageInfo.description || "",
-        saiba_mais: data.pageListaOfertaFormativasPrevista.SaibaMais || [],
+        title: data.pageListaOfertaFormativaArquivada.PageInfo.title || "",
+        subtitle: data.pageListaOfertaFormativaArquivada.PageInfo.subtitle || "",
+        description: data.pageListaOfertaFormativaArquivada.PageInfo.description || "",
+        saiba_mais: data.pageListaOfertaFormativaArquivada.SaibaMais || [],
     };
 };
